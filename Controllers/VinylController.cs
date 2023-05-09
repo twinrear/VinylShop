97b1c08e8e2bc8f75a08db41ce59202e94aaa7dd
twinrear <wladmuse@gmail.com>
2023-05-09 20:49:13 +0200

    page for adding new vinyls added

--- Controllers/VinylController.cs
+++ Controllers/VinylController.cs
@@ -7,16 +7,40 @@ namespace VinylShop.Controllers
     {
         private readonly IVinylRepository vinylRepository;
         private readonly ICategoryRepository categoryRepository;
+        private readonly VinylShopDbContext context;
 
-        public VinylController(IVinylRepository vinylRepository, ICategoryRepository categoryRepository)
+        public VinylController(IVinylRepository vinylRepository, ICategoryRepository categoryRepository, VinylShopDbContext vinylShopDbContext)
         {
             this.vinylRepository = vinylRepository;
             this.categoryRepository = categoryRepository;
+            this.context = vinylShopDbContext;
         }   
 
         public IActionResult List()
         {
             return View(vinylRepository.AllVinyls);
+        }
+
+        public IActionResult AddNewVinyl() 
+        {
+   //         var vinyl = new Vinyl();
+			//vinyl.AlbumName = Request.Form["AlbumName"];
+			//vinyl.ArtistName = Request.Form["ArtistName"];
+			//vinyl.Price = decimal.Parse(Request.Form["Price"]);
+			//vinyl.Description = Request.Form["Description"];
+			//vinyl.IsVinylOfTheWeek = Request.Form.ContainsKey("VinylOfTheWeek");
+			//vinyl.CategoryId = int.Parse(Request.Form["CategoryId"]);
+
+			//context.Vinyls.Add(vinyl);
+   //         context.SaveChanges();
+
+            return View(categoryRepository.AllCategories);
+		}
+
+        [HttpPost]
+        public IActionResult AddVinyl()
+        {
+            return RedirectToAction("List", "Vinyl");
         }
     }
 }
